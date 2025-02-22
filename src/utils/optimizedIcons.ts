82cc21d88fcf308f6f56b3a63791bf6f76d5eb63
TEO <alfredo.ssm@gmail.com>
2025-02-22 13:41:02 -0300

    imports only used icons

--- src/utils/optimizedIcons.ts
+++ src/utils/optimizedIcons.ts
@@ -1,28 +1,36 @@
 import { lazy } from 'react'
-import * as LucideIcons from 'lucide-react'
+import {
+  Code,
+  BarChart,
+  Bot,
+  Mail,
+  Instagram,
+  Phone,
+  Linkedin,
+} from 'lucide-react'
 
 console.log('Netlify env:', import.meta.env.NETLIFY)
 
 const isNetlify = Boolean(import.meta.env.NETLIFY)
 
 // Lista de iconos usados
-const icons = [
-  'Code',
-  'BarChart',
-  'Bot',
-  'Mail',
-  'Instagram',
-  'Phone',
-  'Linkedin',
-]
+const icons = {
+  Code,
+  BarChart,
+  Bot,
+  Mail,
+  Instagram,
+  Phone,
+  Linkedin,
+}
 
 export const Icons = isNetlify
-  ? icons.reduce((acc, name) => {
-      acc[name] = lazy(() =>
+  ? Object.keys(icons).reduce((acc, name) => {
+      acc[name as keyof typeof icons] = lazy(() =>
         import('lucide-react').then((mod) => ({
           default: (mod as Record<string, any>)[name],
         }))
       )
       return acc
     }, {} as Record<string, any>)
-  : LucideIcons
+  : icons
