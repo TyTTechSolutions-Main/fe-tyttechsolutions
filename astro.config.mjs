3ee3148f4c0bdf27d75d71bd77403ab7f79b1d97
TEO <alfredo.ssm@gmail.com>
2025-02-22 01:21:43 -0300

    fix cfg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,26 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify({}),
+  adapter: netlify(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name?.endsWith('.css')) {
+              return '_astro/[name]-[hash][extname]';
+            }
+            return '[name]-[hash][extname]';
+          },
+        },
+      },
+    },
   },
 });
