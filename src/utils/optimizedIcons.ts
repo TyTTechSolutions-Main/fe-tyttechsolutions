73e3b7627638b6e040d659a969864f58557657b4
TEO <alfredo.ssm@gmail.com>
2025-02-22 01:27:53 -0300

    tsconfig change

--- src/utils/optimizedIcons.ts
+++ src/utils/optimizedIcons.ts
@@ -1,15 +1,26 @@
-import { lazy } from 'react';
-import * as LucideIcons from 'lucide-static';
+import { lazy } from 'react'
+import * as LucideIcons from 'lucide-static'
 
-// Verifica si estamos en Netlify (SSR)
-const isNetlify = import.meta.env?.NETLIFY === 'true';
+const isNetlify = import.meta.env?.NETLIFY === 'true'
 
-// Lista de íconos usados
-const icons = ['Code', 'BarChart', 'Bot', 'Mail', 'Instagram', 'Phone', 'Linkedin'];
+// Lista de iconos usados
+const icons = [
+  'Code',
+  'BarChart',
+  'Bot',
+  'Mail',
+  'Instagram',
+  'Phone',
+  'Linkedin',
+]
 
 export const Icons = isNetlify
   ? icons.reduce((acc, name) => {
-      acc[name] = lazy(() => import(`lucide-static/icons/${name}.js`).then((mod) => ({ default: mod.default })));
-      return acc;
+      acc[name] = lazy(() =>
+        import(`lucide-static/icons/${name}.js`).then((mod) => ({
+          default: mod.default,
+        }))
+      )
+      return acc
     }, {} as Record<string, any>)
-  : LucideIcons;
+  : LucideIcons
