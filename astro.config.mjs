c3c1197b5d7441894c5325cbfc216883c6587f8b
TEO <alfredo.ssm@gmail.com>
2025-02-22 18:26:19 -0300

    change adapter import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   output: 'server',
